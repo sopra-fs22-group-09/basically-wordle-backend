f244c63845579efd2b8d74f2a7bde182ff0a3989
jemaie <jerome.maier@uzh.ch>
2022-05-05 18:53:52 +0200

    game logic finally done probably

--- src/main/java/ch/uzh/sopra/fs22/backend/wordlepvp/model/gameModes/WordsPP.java
+++ src/main/java/ch/uzh/sopra/fs22/backend/wordlepvp/model/gameModes/WordsPP.java
@@ -1,7 +1,15 @@
 package ch.uzh.sopra.fs22.backend.wordlepvp.model.gameModes;
 
-import ch.uzh.sopra.fs22.backend.wordlepvp.model.*;
+import ch.uzh.sopra.fs22.backend.wordlepvp.model.GameLogic;
 
 public class WordsPP extends GameLogic {
+
+    public WordsPP() {
+        super();
+        super.setAmountRounds(0);
+        super.setRoundTime(300);
+        super.setMaxRounds(0);
+        super.setMaxTime(900);
+    }
 
 }
---
+++ src/main/java/ch/uzh/sopra/fs22/backend/wordlepvp/util/GameTimerTask.java
@@ -0,0 +1,23 @@
+package ch.uzh.sopra.fs22.backend.wordlepvp.util;
+
+import ch.uzh.sopra.fs22.backend.wordlepvp.model.Game;
+import ch.uzh.sopra.fs22.backend.wordlepvp.repository.GameRepository;
+
+import java.util.TimerTask;
+
+public class GameTimerTask extends TimerTask {
+
+    private final Game game;
+    private final GameRepository gameRepository;
+
+    public GameTimerTask(Game game, GameRepository gameRepository) {
+        this.game = game;
+        this.gameRepository = gameRepository;
+    }
+
+    @Override
+    public void run() {
+        game.endRound();
+        gameRepository.saveGame(game).subscribe();
+    }
+}
