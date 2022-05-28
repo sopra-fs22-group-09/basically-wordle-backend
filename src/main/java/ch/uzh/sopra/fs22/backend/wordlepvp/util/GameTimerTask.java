4f3ff0296a8c91ae087abb8da710c13c325278dc
jemaie <jerome.maier@uzh.ch>
2022-05-28 23:19:46 +0200

    nope

--- src/main/java/ch/uzh/sopra/fs22/backend/wordlepvp/util/GameTimerTask.java
+++ src/main/java/ch/uzh/sopra/fs22/backend/wordlepvp/util/GameTimerTask.java
@@ -1,8 +1,6 @@
 package ch.uzh.sopra.fs22.backend.wordlepvp.util;
 
 import ch.uzh.sopra.fs22.backend.wordlepvp.logic.Game;
-import ch.uzh.sopra.fs22.backend.wordlepvp.model.GameStatus;
-import ch.uzh.sopra.fs22.backend.wordlepvp.model.Player;
 import ch.uzh.sopra.fs22.backend.wordlepvp.repository.GameRepository;
 import ch.uzh.sopra.fs22.backend.wordlepvp.service.GameService;
 import reactor.core.scheduler.Schedulers;
